6e232ea19dfb441a5e169626330c6b56f663a534
Olena <alchimeraa@gmail.com>
2025-01-23 20:02:36 +0200

    src

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -4,11 +4,17 @@ export const getContacts = async (req, res) => {
   console.log("GET /contacts called");
   const contacts = await contactsService.getAllContacts();
   console.log("Found contacts:", contacts);
-  res.status(200).json({
-    status: 200,
-    message: "Successfully found contacts!",
-    data: contacts,
-  });
+  res.status(200).send(
+    JSON.stringify(
+      {
+        status: 200,
+        message: "Successfully found contacts!",
+        data: contacts,
+      },
+      null, // Для перетворення в "в стовпчик"
+      2 // Кількість пробілів для відступів
+    )
+  );
 };
 
 export const getContact = async (req, res) => {
@@ -20,11 +26,17 @@ export const getContact = async (req, res) => {
     return res.status(404).json({ message: "Contact not found" });
   }
   console.log("Found contact:", contact);
-  res.status(200).json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
-  });
+  res.status(200).send(
+    JSON.stringify(
+      {
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      },
+      null, // Для перетворення в "в стовпчик"
+      2 // Кількість пробілів для відступів
+    )
+  );
 };
 
 export default { getContacts, getContact };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,15 +1,5 @@
 import Contact from "../db/contactModel.js";
 
-// const getAllContacts = async () => {
-//   return await Contact.find();
-// };
-
-// const getContactById = async (id) => {
-//   return await Contact.findById(id);
-// };
-
-// export default { getAllContacts, getContactById };
-
 
 const getAllContacts = async () => {
   const contacts = await Contact.find();
