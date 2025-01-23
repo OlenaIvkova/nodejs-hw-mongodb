fad66fe89db5ec374908cccf8a1f0d8e6982e71e
Olena <alchimeraa@gmail.com>
2025-01-23 20:10:05 +0200

    src

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -4,17 +4,11 @@ export const getContacts = async (req, res) => {
   console.log("GET /contacts called");
   const contacts = await contactsService.getAllContacts();
   console.log("Found contacts:", contacts);
-  res.status(200).send(
-    JSON.stringify(
-      {
-        status: 200,
-        message: "Successfully found contacts!",
-        data: contacts,
-      },
-      null, // Для перетворення в "в стовпчик"
-      2 // Кількість пробілів для відступів
-    )
-  );
+  res.status(200).json({
+    status: 200,
+    message: "Successfully found contacts!",
+    data: contacts,
+  });
 };
 
 export const getContact = async (req, res) => {
@@ -26,17 +20,11 @@ export const getContact = async (req, res) => {
     return res.status(404).json({ message: "Contact not found" });
   }
   console.log("Found contact:", contact);
-  res.status(200).send(
-    JSON.stringify(
-      {
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      },
-      null, // Для перетворення в "в стовпчик"
-      2 // Кількість пробілів для відступів
-    )
-  );
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
 };
 
 export default { getContacts, getContact };
