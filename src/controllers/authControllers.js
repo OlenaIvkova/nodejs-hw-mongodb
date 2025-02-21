491004f3a23e675bdc17b06c7db3c9ca5776f219
Olena <alchimeraa@gmail.com>
2025-02-21 21:03:16 +0200

    src

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -51,9 +51,6 @@ export const login = async (req, res, next) => {
     if (!isPasswordValid) {
       throw createHttpError(401, "Invalid email or password");
     }
-
-    // const accessToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15m" });
-    // const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: "30d" });
     
     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "15m" });
     const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: "30d" });
