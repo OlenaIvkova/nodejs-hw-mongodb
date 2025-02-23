557132e5e989177ff42ed47bfc279789f7359639
Olena <alchimeraa@gmail.com>
2025-02-23 10:34:38 +0200

    src

--- nodejs-hw-mongodb/src/routes/contact.js
+++ nodejs-hw-mongodb/src/routes/contact.js
@@ -71,3 +71,26 @@ router.delete("/:id", authenticate, async (req, res, next) => {
 });
 
 export default router;
+
+
+// import express from 'express';
+// import contactsController from '../controllers/contactsController.js';
+// import ctrlWrapper from '../utils/ctrlWrapper.js';
+// import validateBody from '../middlewares/validateBody.js';
+// import contactSchema from '../schemas/contactValidation.js';  
+// import isValidId from '../middlewares/isValidId.js';
+// import authenticate from "../middlewares/authenticate.js"; 
+
+
+// const router = express.Router();
+
+
+// router.use(authenticate); 
+
+// router.get('/', ctrlWrapper(contactsController.getContacts));
+// router.get('/:contactId', isValidId, ctrlWrapper(contactsController.getContact));
+// router.post('/', validateBody(contactSchema), ctrlWrapper(contactsController.createContact));  
+// router.patch('/:contactId', isValidId, validateBody(contactSchema), ctrlWrapper(contactsController.updateContact));  
+// router.delete('/:contactId', isValidId, ctrlWrapper(contactsController.deleteContact));
+
+// export default router;
